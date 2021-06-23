5c9bbedd496cb19bf463a1e77f41824c6e1eebd1
Barnabas Molnar <hanta911@gmail.com>
2021-06-23 02:20:32 +0200

    Frist Commit + Creating Routing

---
+++ src/components/NavMenu.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable prettier/prettier */
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+function NavMenu() {
+    return (
+       <div>
+           <ul>
+               <li>
+                   <NavLink to='/'>Home</NavLink>
+                </li>
+               <li>
+                   <NavLink to='/about'>About</NavLink>
+                </li>
+               <li>
+                   <NavLink to='/projects'>Projects</NavLink>
+                </li>
+               <li>
+                   <NavLink to='/contact'>Contact</NavLink>
+                </li>
+               
+           </ul>
+       </div>
+    )
+}
+
+export default NavMenu
+
